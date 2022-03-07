a30f26fb5ab042a355a5edd500133f7ea62c4abe
jedan2506 <97698934+jedan2506@users.noreply.github.com>
2022-03-07 15:25:07 +0530

    Made some more changes

--- Exercise5/ll.php
+++ Exercise5/ll.php
@@ -3,7 +3,6 @@
    <head>
       <title>Tutorialspoint.com</title>
       <link href = "css/bootstrap.min.css" rel = "stylesheet">
-      
       <style>
          body {
             padding-top: 40px;
@@ -63,12 +62,12 @@
       
    </head>
 	
-   <body>
+   <body onload="rest()">
       
       <h2>Enter Username and Password</h2> 
       <div class = "container form-signin">
          
-         
+
          <?php
          	ob_start();
    			session_start();
@@ -82,6 +81,7 @@
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
                   $_SESSION['username'] = $_POST['username'];
+                  $_SESSION['password'] = $_POST['password'];
                   
                   echo 'You have entered valid use name and password';
                   header('Location: ll1.php');
@@ -89,6 +89,7 @@
                else 
                {
                   echo "<script> alert('Wrong username or password'); </script>";
+                  session_unset();
                   $_SESSION['valid'] = '';
                   $_SESSION['timeout'] = '';
                   $_SESSION['username'] = '';
@@ -102,15 +103,11 @@
          <form class = "form-signin" role = "form" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
             <h4 class = "form-signin-heading"></h4>
             <input type = "text" class = "form-control" name = "username"><br><br>
-            <input type = "password" class = "form-control" name = "password"><br><br>
+            <input type = "password" class = "form-control" name = "password" id="password"><br><br>
             <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "login">Login</button>
          </form>
 			
          Click here to clean <a href = "logout.php" tite = "Logout">Session.<br><br>
-         	<?php
-         	$value=$_SESSION['username'];
-         	echo $value;
-         	?>
          
       </div> 
       
